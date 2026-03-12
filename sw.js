9a8103111ed6f09c055a8ac919136d31186b2c16
nicoferrero11 <nicoferrero11@gmail.com>
2026-03-12 00:38:05 -0300

    Agrego nuevos cambios y PWA

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = 'tabu-argentum-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Inter:wght@300;400;500;600&display=swap'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(ASSETS.filter(function(a){ return !a.startsWith('http'); }));
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request).catch(function() { return caches.match('./index.html'); });
+    })
+  );
+});
